1b0e18b3a7ec104880a07e837e42a7216dc0a8c5
javierenriquemendozagomez@gmail.com <javierenriquemendozagomez@gmail.com>
2020-06-17 21:12:58 +0200

    WIP

--- src/components/ConcertThumbnail.tsx
+++ src/components/ConcertThumbnail.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import {Image, View, StyleSheet} from 'react-native';
-
+import './ConcertThumbnail.scss';
 const styles = StyleSheet.create({
     container: {
       paddingTop: 50,
     },
-    logo: {
-      width: 66,
-      height: 58,
-    },
+    h1Color: {
+        height: 'red'
+    }
 });
 export interface IConcertThumbnailProps {
+    id: string;
     title: string;
     imgURL: string;
 };
 
 const ConcertThumbnail = ({title, imgURL}: IConcertThumbnailProps) => (
     <View>
-        <h1> {title} </h1>
-        <Image
-            style={styles.logo}
-            source={{
-                uri: imgURL,
-            }}
-        />
+        <div className="imageLogo">
+            <Image source={{uri: imgURL}}/>
+        </div>
+        <h1 > {title} </h1>
     </View>
 );
 
--- src/pages/Concerts.tsx
+++ src/pages/Concerts.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import {View} from 'react-native';
 import ConcertThumbnail, { IConcertThumbnailProps } from '../components/ConcertThumbnail';
-
+import { Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button } from 'native-base';
 const Concerts = () => {
     const concertThumbnails: IConcertThumbnailProps [] = [
         {
-            imgURL: 'https://www.nacionrock.com/wp-content/uploads/system-of-a-down-steal-this-album-cd-D_NQ_NP_713898-MLM27074074828_032018-F.jpg%22',
+            id: '7500bfce-fb6d-4c11-8090-05e90c835b1a',
+            imgURL: 'https://ih1.redbubble.net/image.1078595872.7625/flat,128x,075,f-pad,128x128,f8f8f8.jpg',
             title: 'System of a down'
         }
     ]
     return (
         <View>
-            {
-            concertThumbnails.map((concertThumbnailProps) => (
-                <ConcertThumbnail {...concertThumbnailProps} />
-            ))
-            }
+                {
+                concertThumbnails.map((concertThumbnailProps) => (
+                    <ConcertThumbnail {...concertThumbnailProps} key={concertThumbnailProps.id}/>
+                ))}
         </View>
     )
 };
